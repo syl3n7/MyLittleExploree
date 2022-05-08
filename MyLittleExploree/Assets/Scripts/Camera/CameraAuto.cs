3067bbc3e04f5e3dbf68ddb1c5017538f8666b5a
syl3n7 <claudiosilva@protonmail.com>
2022-05-08 12:38:53 +0100

    update map, sounds, few tweaks
    tweaks include separating the sprites from the tilemaps to make it independent objects to be manipulated with code
    
    Co-Authored-By: Diogo <53532032+Di0go@users.noreply.github.com>
    Co-Authored-By: Mariana041 <100358775+Mariana041@users.noreply.github.com>
    Co-Authored-By: Serra2323 <96260039+Serra2323@users.noreply.github.com>
    Co-Authored-By: KlausJust <105154977+KlausJust@users.noreply.github.com>

--- MyLittleExploree/Assets/Scripts/Camera/CameraAuto.cs
+++ MyLittleExploree/Assets/Scripts/Camera/CameraAuto.cs
@@ -2,16 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Camera))]
 public class CameraAuto : MonoBehaviour
 {
     public List<Transform> targets;
 
     public Vector3 offset;
     public float smoothTime = .5f;
+    
+    public float minZoom = 40f;
+    public float maxZoom = 10f;
+    public float zoomLimiter = 50f;
 
     private Vector3 velocity;
-    
+    private Camera cam;
+
+    private void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
     void LateUpdate()
+    {
+        if(targets.Count == 0)
+            return;
+        Move();
+        Zoom();
+    }
+    void Zoom()
+    {
+        float newZoom = Mathf.Lerp(maxZoom, minZoom, GetGreatestDistance() / zoomLimiter);
+        cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, newZoom, Time.deltaTime);
+    }
+    void Move()
     {
         Vector3 centerPoint = GetCenterPoint();
 
@@ -20,7 +42,15 @@ public class CameraAuto : MonoBehaviour
         transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref velocity, smoothTime);
     }
    
-
+    float GetGreatestDistance()
+    {
+        var bounds = new Bounds(targets[0].position, Vector2.zero);
+        for(int i = 1; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+        return bounds.size.x;
+    }
 
     Vector3 GetCenterPoint()
     {
